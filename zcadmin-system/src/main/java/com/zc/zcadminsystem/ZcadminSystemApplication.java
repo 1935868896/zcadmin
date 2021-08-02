071caa560d6a56f62d7adc3c423f5094841cd486
ZhangChen <5178078+zc17862905520@user.noreply.gitee.com>
2021-08-02 14:40:44 +0800

    first commit

---
+++ zcadmin-system/src/main/java/com/zc/zcadminsystem/ZcadminSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zc.zcadminsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZcadminSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZcadminSystemApplication.class, args);
+    }
+
+}
---
+++ zcadmin-system/src/test/java/com/zc/zcadminsystem/ZcadminSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zc.zcadminsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZcadminSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
