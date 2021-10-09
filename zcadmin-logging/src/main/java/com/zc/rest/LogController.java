4810447812639cc4dabfbb7e2003dc648b5f2c89
ZhangChen <1935868896@qq.com>
2021-10-09 14:19:09 +0800

    日志查询 post请求

--- zcadmin-logging/src/main/java/com/zc/rest/LogController.java
+++ zcadmin-logging/src/main/java/com/zc/rest/LogController.java
@@ -25,10 +25,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Zheng Jie
@@ -42,70 +39,19 @@ public class LogController {
 
     private final ISysLogService logService;
 
-//    @Log("导出数据")
-//    @ApiOperation("导出数据")
-//    @GetMapping(value = "/download")
-//    @PreAuthorize("@el.check()")
-//    public void download(HttpServletResponse response, LogQueryCriteria criteria) throws IOException {
-//        criteria.setLogType("INFO");
-//        logService.download(logService.queryAll(criteria), response);
-//    }
-//
-//    @Log("导出错误数据")
-//    @ApiOperation("导出错误数据")
-//    @GetMapping(value = "/error/download")
-//    @PreAuthorize("@el.check()")
-//    public void downloadErrorLog(HttpServletResponse response, LogQueryCriteria criteria) throws IOException {
-//        criteria.setLogType("ERROR");
-//        logService.download(logService.queryAll(criteria), response);
-//    }
-    @GetMapping
+
+    @PostMapping
     @ApiOperation("日志查询")
-//    @PreAuthorize("@el.check()")
-    public ResultResponse query(SysLog log, Page page){
+    public ResultResponse query(SysLog log, @RequestBody Page page){
         IPage<SysLog> sysLogIPage = logService.selectByLogType(page, log.getLogType());
 
         return ResultResponse.success(sysLogIPage);
     }
 
-//    @GetMapping(value = "/user")
-//    @ApiOperation("用户日志查询")
-//    public ResponseEntity<Object> queryUserLog(LogQueryCriteria criteria, Pageable pageable){
-//        criteria.setLogType("INFO");
-//        criteria.setBlurry(SecurityUtils.getCurrentUsername());
-//        return new ResponseEntity<>(logService.queryAllByUser(criteria,pageable), HttpStatus.OK);
-//    }
-//
-//    @GetMapping(value = "/error")
-//    @ApiOperation("错误日志查询")
-//    @PreAuthorize("@el.check()")
-//    public ResponseEntity<Object> queryErrorLog(LogQueryCriteria criteria, Pageable pageable){
-//        criteria.setLogType("ERROR");
-//        return new ResponseEntity<>(logService.queryAll(criteria,pageable), HttpStatus.OK);
-//    }
-//
     @GetMapping(value = "/error/{id}")
     @ApiOperation("日志异常详情查询")
-//    @PreAuthorize("@el.check()")
     public  ResultResponse queryErrorLogs(@PathVariable Long id){
         return ResultResponse.success(logService.findByErrDetail(id));
     }
 
-//    @DeleteMapping(value = "/del/error")
-//    @Log("删除所有ERROR日志")
-//    @ApiOperation("删除所有ERROR日志")
-//    @PreAuthorize("@el.check()")
-//    public ResponseEntity<Object> delAllErrorLog(){
-//        logService.delAllByError();
-//        return new ResponseEntity<>(HttpStatus.OK);
-//    }
-//
-//    @DeleteMapping(value = "/del/info")
-//    @Log("删除所有INFO日志")
-//    @ApiOperation("删除所有INFO日志")
-//    @PreAuthorize("@el.check()")
-//    public ResponseEntity<Object> delAllInfoLog(){
-//        logService.delAllByInfo();
-//        return new ResponseEntity<>(HttpStatus.OK);
-//    }
 }
